340f8f74041d5802eb6ffb13bed83d681012ccee
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2023-03-01 23:07:43 +0300

    add new tasks

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+        static void OutputArray(Array array)
+        {
+
+        }
+
+        static void AddArray(object obj)
+        {
+
+        }
+
+        static void DeleteArray(object obj)
+        {
+
+        }
+
+        static Array CreateArray(int elementCount)
+        {
+            throw new Exception("функция не реализована");
+        }
+    }
+}
