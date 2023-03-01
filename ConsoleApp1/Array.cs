33803d3fc37d382c93928a8269aff746fa672e95
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2023-03-01 23:07:43 +0300

    add new tasks

---
+++ ConsoleApp1/Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Array<T>
+    {
+        private T[] array;
+        public void OutputArray(T[] array)
+        {
+
+        }
+
+        public void AddArray(T obj)
+        {
+
+        }
+
+        public void DeleteArray(T obj)
+        {
+
+        }
+
+        public T[] CreateArray(int elementCount)
+        {
+            throw new Exception("функция не реализована");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,27 +6,20 @@ namespace Console1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            ConsoleApp1.Array<int> array= new ConsoleApp1.Array<int>();
+            Console.WriteLine("Введите кол-во элементов нового массива");
+            var hui = array.CreateArray(int.Parse(Console.ReadLine() ?? "0"));
+            array.OutputArray(hui);
+            Console.WriteLine("Введите элемент для добавления в массив");
+            var addItem = int.Parse(Console.ReadLine() ?? "0");
+            array.AddArray(addItem);
+            array.OutputArray(hui);
+            Console.WriteLine("Введите индекс удаляемого элемента массива");
+            var deleteItem = int.Parse(Console.ReadLine() ?? "0");
+            array.DeleteArray(deleteItem);
+            array.OutputArray(hui);
         }
 
-        static void OutputArray(Array array)
-        {
-
-        }
-
-        static void AddArray(object obj)
-        {
-
-        }
-
-        static void DeleteArray(object obj)
-        {
-
-        }
-
-        static Array CreateArray(int elementCount)
-        {
-            throw new Exception("функция не реализована");
-        }
+       
     }
 }
